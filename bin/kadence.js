805540ebac66377e4141fb1ddafe9ecd3a211db4
LevelUpWithAlex <97421537+Down2EarthGG@users.noreply.github.com>
2026-02-05 20:44:15 -0800

    feat: Add Kadence CLI for compiling, running, and managing projects and packages, and update package.json.

--- bin/kadence.js
+++ bin/kadence.js
@@ -5,7 +5,7 @@ const readline = require("readline");
 const { execSync } = require("child_process");
 const { compile } = require("../src/compiler");
 
-const VERSION = "0.2.1";
+const VERSION = "0.2.2";
 
 const colors = {
   reset: "\x1b[0m",
